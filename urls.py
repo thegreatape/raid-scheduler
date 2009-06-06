c79a2540a2fd3395d9e8d4cc09987384af9d28ee
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2009-06-06 00:43:43 +0000

    fix urls for apache deployment

--- urls.py
+++ urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^raid-scheduler/', include('raid_scheduler.raid_calendar.urls')),
-	(r'^raid-scheduler/login$', 'django.contrib.auth.views.login', {'template_name': 'home/login.djhtml'} ),
-	(r'^raid-scheduler/logout$', 'django.contrib.auth.views.logout' ),
-	(r'^raid-scheduler/admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^raid-scheduler/admin/(.*)', admin.site.root),
+    (r'', include('raid_scheduler.raid_calendar.urls')),
+    (r'^login$', 'django.contrib.auth.views.login', {'template_name': 'home/login.djhtml'} ),
+    (r'^logout$', 'django.contrib.auth.views.logout' ),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/(.*)', admin.site.root),
 )
