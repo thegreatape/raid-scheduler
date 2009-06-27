b7e0a6ee37cb151b7ea3e48b895bf0c67bc2a57e
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2009-06-27 14:29:38 -0400

    controls for paging through different months of raid events

--- raid_calendar/views.py
+++ raid_calendar/views.py
@@ -7,14 +7,19 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.core.urlresolvers import reverse
 
+import calendar
 from calendar import Calendar
 from forms import RegistrationForm
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 from raid_calendar.models import Raid, Registration
 
 def home(request):
 	today = date.today()
-	raids = Raid.objects.filter(date__gte=datetime(today.year, today.month, 1))
+	if request.method == 'GET' and 'year' in request.GET or 'month' in request.GET:
+		today = date(year=int(request.GET.get('year', today.year)), month=int(request.GET.get('month', today.month)), day=today.day)
+
+	days_in_month = calendar.mdays[today.month]
+	raids = Raid.objects.filter(date__gte=datetime(today.year, today.month, 1)).exclude(date__gte=datetime(today.year, today.month, days_in_month))
 	days = [{'day': x} for x in sum(Calendar().monthdayscalendar(today.year, today.month), [])]
 
 	for raid in raids:
@@ -23,9 +28,17 @@ def home(request):
 			event["raids"].append(raid)
 		else:
 			event["raids"] = [raid]
-
+	one_month = timedelta(days=days_in_month)
+	forward = today + one_month
+	back = today - one_month
+	qstring = lambda d: 'month=%i&year=%i' % (d.month, d.year)
+	
 	context = {'date': today.strftime('%B %Y'),
-			   'days' : days}
+		   'days' : days,
+		   'nav' : {'forward': {'title': 'July 2009', 'qstring': qstring(forward), 'label': forward.strftime('%B %Y')},
+			    'back'  : {'title': 'May 2009', 'qstring': qstring(back), 'label': back.strftime('%B %Y')}
+			    }
+		   }
 	return render_to_response('home/calendar.djhtml', context, context_instance=RequestContext(request))
 
 @login_required
@@ -35,8 +48,8 @@ def view_raid(request, raid_id):
 		form = RegistrationForm(request.POST)
 		if form.is_valid():
 			registration = Registration(player=request.user,
-										raid=raid,
-										role=form.cleaned_data['role'])
+						    raid=raid,
+						    role=form.cleaned_data['role'])
 			if 'standby' in form.cleaned_data:
 				registration.standby = form.cleaned_data['standby'] 
 			registration.save()
@@ -60,12 +73,12 @@ def view_raid(request, raid_id):
 			'Healers': (('registered', healers),)
 			}
 	return render_to_response('raid/view.djhtml',
-							  {'raid': raid,
-							   'registered': registered,
-							   'registration_form': form,
-							   'is_registered': raid.is_registered(request.user)
-							   },
-							  context_instance=RequestContext(request))
+				  {'raid': raid,
+				   'registered': registered,
+				   'registration_form': form,
+				   'is_registered': raid.is_registered(request.user)
+				   },
+				  context_instance=RequestContext(request))
 
 def create_user(request):
 	if request.method == 'POST':
